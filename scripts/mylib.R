5e91e334ebc4a219081b52f303020d11f7d146de
Yanyu Liang <yanyul@uchicago.edu>
2018-06-15 17:57:48 -0500

    added distance analysis‘

--- scripts/mylib.R
+++ scripts/mylib.R
@@ -10,3 +10,26 @@ get_upper_tri <- function(cormat){
   return(cormat)
 }
 # end
+
+getDistance <- function(pos, bed) {
+  e <- apply(pos, 1, function(x) {
+    chr <- x[1]
+    bed.sub <- bed[bed[,1] == chr, ]
+    if(nrow(bed) == 0) {
+      return(NA)
+    }
+    return(min(abs(bed.sub[, c(2, 3)] - as.numeric(x[2]))))
+  })
+  return(e)
+}
+
+getDistanceToTSS <- function(x) {
+  # x: chr, pos, gene.chr, gene.start, gene.end, strand
+  tts <- x[, 4]
+  neg.ind <- x[, 6] == '-' | x[, 6] == -1
+  tts[neg.ind] <- x[neg.ind, 5]
+  d <- x[, 2] - tts
+  d[neg.ind] <- -d[neg.ind]
+  d[x[, 1] != x[, 3]] <- NA
+  return(d)
+}
