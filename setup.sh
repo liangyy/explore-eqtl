f8e13cabba8f67570bd039539567bb3cae54d17b
Yanyu Liang <yanyul@midway2-login1.rcc.local>
2018-06-25 16:59:27 -0500

    updated config

---
+++ setup.sh
@@ -0,0 +1,14 @@
+# meme setup
+export PATH=$PATH:$HOME/bin/meme/bin:$HOME/bin/meme/libexec/meme-5.0.1
+# END
+
+# bedtools setup
+export PATH=/project2/xinhe/yanyul/softwares/bedtools2/bin:$PATH
+# END
+
+# conda setup
+export PATH=/project2/xinhe/yanyul/softwares/Anaconda2/bin:$PATH
+# END
+
+alias eqtl='source activate explore-eqtl'
+
