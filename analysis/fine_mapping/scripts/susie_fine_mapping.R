3227ac8cc98162dcbd3f301ea0e18403c4c5e3b7
Yanyu Liang <yanyul@andrew.cmu.edu>
2018-07-05 11:03:55 -0500

    fixed minor bug

--- analysis/fine_mapping/scripts/susie_fine_mapping.R
+++ analysis/fine_mapping/scripts/susie_fine_mapping.R
@@ -3,6 +3,6 @@
 # z: n-by-k matrix
 fineMapping <- function(x, y, z) {
   y <- .lm.fit(z, y)$residuals
-  obj <- susieR::susie(x, y, L = 5, estimate_residual_variance = TRUE, prior_variance = 0.1, intercept = FALSE, tol = 1e-3)
+  obj <- susieR::susie(x, y, L = 5, estimate_residual_variance = TRUE, prior_variance = 0.1, intercept = TRUE, tol = 1e-3)
   return(obj)
 }
