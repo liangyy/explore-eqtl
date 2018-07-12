6bff07fbe84c58ec97335f2e92577eae72e4621a
Yanyu Liang <yanyul@andrew.cmu.edu>
2018-07-12 17:19:50 -0500

    fixed wrong name

--- analysis/fine_mapping/scripts/susie_fine_mapping_pv_0.01.R.R
+++
@@ -1,8 +0,0 @@
-# x: n-by-p matrix
-# y: n-by-1 vector
-# z: n-by-k matrix
-fineMapping <- function(x, y, z) {
-  y <- .lm.fit(z, y)$residuals
-  obj <- susieR::susie(x, y, L = 5, estimate_residual_variance = TRUE, prior_variance = 0.01, intercept = TRUE, tol = 1e-3)
-  return(obj)
-}
